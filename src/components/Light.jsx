544623ff917e353e79b9e03e054a28a224739c69
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2018-11-28 09:44:00 -0600

    Add prop-types

--- src/components/Light.jsx
+++ src/components/Light.jsx
@@ -1,4 +1,10 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 export const Light = ({ light, on }) =>
   <div className={`light${on ? ' on' : ''}`} id={light} />
+
+Light.propTypes = {
+  light: PropTypes.string.isRequired,
+  on: PropTypes.bool.isRequired,
+}
